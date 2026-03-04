5b1144b1502af4fa790fc0dd02a26d984905ff82
dhegnaas <abdirahman460930@gmail.com>
2026-03-04 13:49:27 +0300

    first commit

---
+++ 1-vriables-data-types/01-console/script.js
@@ -0,0 +1,31 @@
+//log number
+console.log(123);
+
+//log string
+console.log("hello daaminnkeyga");
+
+//log multiple
+console.log( 1234, "haye muuse", true)
+
+//log variablr 
+const greeting = "hello everyone";
+console.log(greeting);
+
+//log error and warning
+console.error("this is error");
+console.warn("this is warning");
+
+//log object as table
+console.table({ name: "Abdirahman", email: "abdirahman@gmail.com" })
+
+// group console
+console.group("Group 1");
+console.log(greeting);
+console.log(123);
+console.error("this is n error");
+console.groupEnd();
+
+console.log("hello")
+
+const styles = "padding:10px; background-color:red; color:white font-style:italic;";
+console.log("%c ii warama saaxibayaal", styles);
