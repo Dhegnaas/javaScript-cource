0ddec6cb73ccd0913aaf6a93bf333e0de7baa2c7
dhegnaas <abdirahman460930@gmail.com>
2026-03-07 10:58:37 +0300

    3-variables

---
+++ 3-variables/script.js
@@ -0,0 +1,33 @@
+// 1 const
+
+// const firstName = "hassan";
+const firstName = "sabca";
+// 
+
+// 2 let
+
+let lastName = "mr bean";
+// let lastName = "mr biin";
+lastName = "mr biin";
+
+var phone = "+252 617614133";
+
+var phone = "+252 61616688";
+
+phone = "617624133";
+
+const temp = 37;
+
+const x = 10;
+const y = 7;
+const z = x + y;
+
+let num1;
+num1 = 2;
+
+let a, b, c;
+a = 10;
+b = 5;
+c = 2;
+
+console.log(a+b+c);
