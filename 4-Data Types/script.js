bd8114675c1c8cd37d7a59b43878ce0855a4ebb6
dhegnaas <abdirahman460930@gmail.com>
2026-03-07 12:17:47 +0300

    data types & tpe conversition

---
+++ 4-Data Types/script.js
@@ -0,0 +1,29 @@
+// premative data types
+
+// string
+const intro = "ku soo dhawaaw barashada data type";
+
+//number
+const age = 25;
+const salary = 200.00;
+// boolean
+const isAdmin =true;
+
+// undefined
+let score;
+// null
+let name = "cabdala";
+name = null;
+
+// bigint 
+const number = 1n;
+
+//references data types
+const numbers = [1, 2, 3, 4, 5];
+
+const person = {
+    name: "sacba ka sacba",
+    email: "sacba@gmail.com",
+};
+
+console.log(typeof person);
---
+++ 5 type conversition/script.js
@@ -0,0 +1,23 @@
+// type conversion
+let myvalue = "225524farayare";
+
+// 1 conversion string into number
+// myvalue = parseInt(myvalue);
+// myvalue = +myvalue;
+// myvalue = Number(myvalue);
+
+// 2 convert string into decimal
+// myvalue = parseFloat(myvalue);
+
+// 3 converting number to string 
+// myvalue = myvalue.toString();
+// myvalue = String(myvalue);
+
+// 4 converting number to boolean
+// myvalue = Boolean(myvalue);
+
+// 5 converting boolean to number
+// myvalue = Number(myvalue);
+
+
+console.log(myvalue, typeof myvalue);
