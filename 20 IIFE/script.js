42292f10440a11ae243882de9a0679d84cf6b8d1
dhegnaas <abdirahman460930@gmail.com>
2026-03-10 14:51:02 +0300

    IIFE & IF Statement

---
+++ 20 IIFE/script.js
@@ -0,0 +1,11 @@
+// IIFE Immediately Invoked Function Expression
+
+(function sayHello(){
+    console.log("hello teacher");
+})();
+
+(function greeting(time) {
+    console.log(`good ${time} students`);
+}) ("morning");
+
+
---
+++ 21 if statement/script.js
@@ -0,0 +1,27 @@
+// example 1
+
+if (5 > 3) {
+    console.log("5 is greater than 3");
+}
+
+// example 2
+const number1 = 50;
+const number2 = 47;
+if (number1 > number2) {
+    console.log(`${number1} is greater than ${number2}`);
+}
+
+//example 3
+const isLoggedIn = true;
+if (isLoggedIn) {
+    console.log("welcome, you are logged in");
+}
+
+// example 4
+const points =55;
+
+if (points > 50 ) {
+    console.log("you are passed")
+}  
+
+
