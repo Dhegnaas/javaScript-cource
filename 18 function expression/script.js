8da5b0686303c23f0b5489bc730e44494a454e43
dhegnaas <abdirahman460930@gmail.com>
2026-03-10 14:14:29 +0300

    function expression & arrow function

---
+++ 18 function expression/script.js
@@ -0,0 +1,13 @@
+// function expression 
+const introduce = function (name, age) {
+    console.log(`my name is ${name} and i am ${age} years old`);
+};
+
+introduce("Abdirahman" , 25);
+
+// function declaration
+function syHello() {
+    console.log("hello teacher");
+}
+
+syHello();
---
+++ 19 Arrow function/script.js
@@ -0,0 +1,31 @@
+// normal declaration
+
+// function syHello() {
+//     //code
+// }
+
+
+//funcion expression
+
+// const syHello = function() {
+//     code
+// }
+
+// arrow function
+
+const sayHello = () => {
+    console.log("hello students");
+};
+
+sayHello();
+
+
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+const subtract = (num1, num2) => num1 - num2;
+
+const divide = (x) => x / 2;
+
+console.log(divide(10));
