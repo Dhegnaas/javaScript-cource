4ba80d527acf852c3e93123095b739358135609e
dhegnaas <abdirahman460930@gmail.com>
2026-03-11 14:32:59 +0300

    tenary operator & for loop

---
+++ 25 tenary operator/script.js
@@ -0,0 +1,16 @@
+
+const age = 9;
+
+// if (age >= 18) {
+//     console.log("you can vote");
+// } else {
+//     console.log("you can not vote");
+// }
+
+age >=18 ? console.log("you can vote") : console.log("you can not vote");
+
+
+const isLoggedIn = false;
+isLoggedIn
+ ? console.log("welcome to the dashboard")
+ : console.log("please login first");
---
+++ 26 for loop/script.js
@@ -0,0 +1,22 @@
+
+
+//   Syntax
+//   for (initialization; condition; increment)
+//   {
+//     // code to excute
+//   }
+
+// example 1
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} x 2 = ${i * 2}`);
+}
+
+//example 2
+// const names = ["abdirahman","mohamed","mohamuud","hassan","aadan"];
+// console.log(names[4]);
+// console.log(names.length);
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i])
+// }
