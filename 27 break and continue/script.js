a4b4e8719db90d1dbed3ebf335efae089b5533d5
dhegnaas <abdirahman460930@gmail.com>
2026-03-13 21:30:20 +0300

    while and do while

---
+++ 27 break and continue/script.js
@@ -0,0 +1,11 @@
+// break and continue
+
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
+
+console.log("=====")
+for (let i = 0; i <= 6; i++) {
+    if (i === 3) break;
+  console.log(i);
+}
---
+++ 28 while and do while/script.js
@@ -0,0 +1,25 @@
+
+
+let i = 7;
+
+// while (i < 9) {
+//     console.log(i);
+//     i++;
+// }
+
+// loop over arrays
+
+// const numbers = [10, 20, 30, 40 ,50, 60,70,80,90];
+
+// while (i < numbers.length) {
+//     console.log(numbers[i]);
+//     i++;
+// }
+
+
+// do while loop
+// - always runs once 
+do {
+    console.log(i);
+    i++;
+}while (i <6);
